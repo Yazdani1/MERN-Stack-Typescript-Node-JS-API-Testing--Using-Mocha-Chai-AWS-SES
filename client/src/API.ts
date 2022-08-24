112ba3d466ea24f703546704350662997292a433
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-24 21:26:01 +0300

    Deploy

--- client/src/API.ts
+++ client/src/API.ts
@@ -16,14 +16,14 @@ export interface CreateUserProps {
 }
 
 export const createUser = async ( props: CreateUserProps): Promise<CreateUserInfo> => {
-  const res = await axios.post("/api/postuser", { ...props });
+  const res = await axios.post(API_URL+"/postuser", { ...props });
   return res.data;
 };
 
 // to get all user list
 
 export const getAlluserList = async () => {
-  const res = await axios.get("/api/getuserlist");
+  const res = await axios.get(API_URL+"/getuserlist");
   return res;
 };
 
@@ -36,20 +36,20 @@ export interface UpdateUserProps {
 }
 
 export const updateUserInfo = async (id: number, props: UpdateUserProps) => {
-  const res = await axios.put("/api/updateuser/" + id, { ...props });
+  const res = await axios.put(API_URL+"/updateuser/" + id, { ...props });
   return res.data;
 };
 
 // to get single user to show in the update form
 
 export const getSingleUser = async (id: number) => {
-  const res = await axios.get("/api/singleuser/" + id);
+  const res = await axios.get(API_URL+"/singleuser/" + id);
   return res;
 };
 
 // to delete user
 
 export const deleteUser = async (id: number) => {
-  const res = await axios.delete("/api/deleteuser/" + id);
+  const res = await axios.delete(API_URL+"/deleteuser/" + id);
   return res;
 };
