0e17f3a083bb17375b976ed69aaf7d96f24238ca
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-24 21:07:05 +0300

    Database

--- model/db.js
+++ model/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGODBNOTE,{
+mongoose.connect(process.env.MONGODB,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 },(err)=>{
