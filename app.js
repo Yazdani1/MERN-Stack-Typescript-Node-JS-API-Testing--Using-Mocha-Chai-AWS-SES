c6132bddce0da5cc999397fffac46d8edd377a02
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-26 20:56:02 +0300

    deploy to heroku

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/api",require("./router/user"));
 if (process.env.NODE_ENV == "production") {
   const path = require("path");
 
-  app.get("/", (req, res) => {
+  app.get("/*", (req, res) => {
     app.use(express.static(path.resolve(__dirname, "client", "build")));
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
